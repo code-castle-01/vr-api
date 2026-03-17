8c122ad5e98c7de4caaa48067a6d9b631254a943
Jorge Castillo <jj.jorgecastillo@gmail.com>
2026-03-16 23:37:37 -0500

    feat deploy

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,14 @@
-export default [
+export default ({ env }) => [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: env.array('CORS_ORIGIN', ['*']),
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
